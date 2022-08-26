6f29d6febf370ad6fad71f41cf836b634cb9a8ac
Alia12345678920 <111192996+Alia12345678920@users.noreply.github.com>
2022-08-26 21:54:23 +0300

    Add files via upload

---
+++ Q3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Q3 {
+    public static void main(String[] args) {
+        int a;
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter the number: ");
+        a=input.nextInt();
+        if (a>0)
+            System.out.println("positive.");
+        else if (a<0)
+            System.out.println("negative.");
+        else System.out.println("zero");
+    }
+}
